08b4501af1528152a3c32f2b2ea859ca552ea356
MikeMull10 <50462800+MikeMull10@users.noreply.github.com>
2023-12-06 14:56:04 -0500

    .

---
+++ music.js
@@ -0,0 +1,55 @@
+const music_list = [
+    {
+        title: "Deck the Halls",
+        artist: "Nat King Cole",
+        music: "music/Deck the Halls - Nat King Cole.mp3",
+        img: "imgs/NatKing"
+    }
+]
+
+const shuffle_music = music_list.sort((a, b) => 0.5 - Math.random());
+
+let song = document.querySelector(".song");
+let album = document.querySelector(".album");
+
+console.log(song);
+console.log(album);
+
+let curr_track = document.createElement('audio');
+
+let track_index = 0;
+let isPlaying = false;
+let isRandom = false;
+let updateTimer;
+
+loadTrack(track_index);
+
+function loadTrack(track_index) {
+    clearInterval(updateTimer);
+
+    curr_track.src = shuffle_music[track_index].music;
+    curr_track.load();
+
+    album.src = shuffle_music[track_index].img;
+
+    song = shuffle_music[track_index].title + " by " + shuffle_music[track_index].artist;
+
+    // updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener('ended', nextTrack);
+}
+
+function nextTrack() {
+    if (track_index < shuffle_music.length - 1) {
+        track_index += 1;
+    } else {
+        track_index = 0;
+    }
+    loadTrack(track_index);
+    playTrack();
+}
+
+function playTrack() {
+    curr_track.play();
+    isPlaying = true;
+}
